5501d6439deb7069064d0e07f660d4dd7528e8bd
Mohnaa <mohnaag@gmail.com>
2024-03-04 23:53:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
+export default function App(){
+  
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title){
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos, {id: crypto.randomUUID(),
+        title , completed: false},
+      ]
+    })
+  }
+  function toggleTodo(id, completed){
+    setTodos(currentTodos =>{
+      return currentTodos.map(todo=>{
+        if(todo.id ===id){
+          return {...todo,completed}
+        }
+
+        return todo
+      }) 
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos =>{
+      return currentTodos.filter(todo =>todo.id !==id)
+    })
+  }
+  return (
+    <>
+    <NewTodoForm onSubmit={addTodo}/>
+    <h1 className="header">TODO LIST</h1>
+    <TodoList todos ={todos}
+    toggleTodo={toggleTodo} 
+    deleteTodo={deleteTodo}/>
+  </>
+  // above is a fragmet which is essentialy an empty tag
+  )
+}
